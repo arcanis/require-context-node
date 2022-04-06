d31db4ced754424a726de490e8d77e64547477dc
Maël Nison <nison.mael@gmail.com>
2022-04-06 19:41:06 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const fs = require(`fs`);
+const path = require(`path`);
+const Module = require(`module`);
+const {SyncFileSystem, ResolverFactory} = require(`enhanced-resolve`);
+
+const asyncModes = new Set([`lazy-once`, `lazy`, `async-weak`]);
+
+const resolver = ResolverFactory.createResolver({
+  useSyncFileSystemCalls: true,
+	fileSystem: fs,
+  resolveToContext: true,
+});
+
+const makeRequireContext = module => {
+  return (request, recursive, filter, mode) => {
+    const resolvedRoot = resolver.resolveSync({}, path.dirname(module.filename), request, {});
+    const keys = new Set();
+
+    function traverseDirectory(directory) {
+      const listing = fs.readdirSync(path.join(resolvedRoot, directory), {
+        withFileTypes: true,
+      });
+
+      for (const entry of listing) {
+        const entryRelativePath = `${directory}/${entry.name}`;
+
+        if (entry.isDirectory()) {
+          if (recursive) {
+            traverseDirectory(entryRelativePath);
+          }
+        } else {
+          if (filter.test(entryRelativePath)) {
+            keys.add(entryRelativePath);
+          }
+        }
+      }
+    }
+
+    traverseDirectory(`.`);
+
+    const requireContextEntry = key => {
+      if (!keys.has(key))
+        throw new Error(`Missing key`);
+
+      const requirePath = path.join(resolvedRoot, key);
+
+      if (asyncModes.has(mode)) {
+        return Promise.resolve().then(() => require(requirePath));
+      } else {
+        return require(requirePath);
+      }
+    };
+
+    requireContextEntry.keys = () => {
+      return [...keys];
+    };
+
+    return requireContextEntry;
+  };
+};
+
+if (!require.main) {
+  globalThis.makeRequireContext = makeRequireContext;
+  Module.wrapper[0] += `require.context = global.makeRequireContext(module);`;
+}
+
+module.exports = makeRequireContext;
---
+++ test.js
@@ -0,0 +1,1 @@
+console.log(require.context(`./`, true, /^\.\/\.yarn\/cache\//).keys())
